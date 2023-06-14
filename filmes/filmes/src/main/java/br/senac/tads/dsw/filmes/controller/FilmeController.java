077b1b1ab7598e3282b2c46b51e803cc70f85455
Matheus Galvão <matheus.gs636@gmail.com>
2023-06-14 20:32:45 -0300

    commit de algo feito

--- filmes/filmes/src/main/java/br/senac/tads/dsw/filmes/controller/FilmeController.java
+++ filmes/filmes/src/main/java/br/senac/tads/dsw/filmes/controller/FilmeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.senac.tads.dsw.filmes.model.Filme;
 import br.senac.tads.dsw.filmes.repository.FilmeRepository;
@@ -14,25 +15,40 @@ import br.senac.tads.dsw.filmes.repository.FilmeRepository;
 @Controller
 @RequestMapping("/filmes")
 public class FilmeController {
-    
+
     @Autowired
     private FilmeRepository filmeRepository;
 
     @GetMapping("/listar")
-    public String listarFIlmes(Model model){
-        model.addAttribute("filmes", filmeRepository.findAll());
-        return "filmes/listar";
+    public ModelAndView listarFilmes(Model model) {
+        ModelAndView mv = new ModelAndView("listagem.html");
+        mv.addObject("filmes", filmeRepository.findAll());
+
+        // Verifica se existe uma mensagem de sucesso no modelo
+        if (model.containsAttribute("mensagemSucesso")) {
+            mv.addObject("mensagemSucesso", model.getAttribute("mensagemSucesso"));
+        }
+
+        return mv;
     }
 
+    // @GetMapping("/listar")
+    // public String listarFIlmes(Model model){
+    // model.addAttribute("filmes", filmeRepository.findAll());
+    // return "filmes/listar";
+    // }
+
     @GetMapping("/novo")
-    public String exibirFormularioNovo(Model model){
+    public ModelAndView exibirFormularioNovo(Model model) {
+        ModelAndView mv = new ModelAndView("/formulario");
         model.addAttribute("filme", new Filme());
-        return "filmes/formulario";   
+        return mv;
     }
 
     @PostMapping("/novo")
-    public String salvarFilme(@ModelAttribute("filme") Filme filme){
+    public String salvarFilme(@ModelAttribute("filme") Filme filme) {
         filmeRepository.save(filme);
-        return "redirect:/filmes";
+        return "redirect:/filmes/listar";
     }
+
 }
